5e8449a7b0be2a6700ad34aaf41574535da2dd52
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-10-19 18:12:23 +0300

    Начал работу над заданием 5 по теме Классы и объекты.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask5
+{
+    class Program
+    {
+        static void Main()
+        {
+            OneDArray array = new OneDArray(4);   // Задаем размер одномерного массива
+            array.InputIntArray();                // Ввод элементов массива с клавиатуры
+            Console.WriteLine("\nИсходный массив:");
+            array.ShowIntArray();                 // Вывод на экран исходных элементов массива
+
+            array.SortingArray();                 // Сортировка элементов массива в порядке возростания
+            Console.WriteLine("\nОтсортированный в порядке возростания массив:");
+            array.ShowIntArray();                 // Вывод на экран отсортированных элементов массива
+
+            Console.ReadKey();
+        }
+    }
+}
